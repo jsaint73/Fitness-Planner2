4d00ab69c688acba2d13fd311fed32114e061819
James Saintilus <jamessaintilus@gmail.com>
2026-07-22 12:53:09 -0400

    Created Components  for react and imported fanta css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fitness Planner</h1>
+      {/* Add your components and routes here */}
+    </div>
+  );
+}
+
+export default App;
